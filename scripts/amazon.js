4c2a1f443483ba02e2c369494a3dc455e314b697
zeyadbadawyy <zeyadbadawyy@gmail.com>
2026-03-03 17:21:15 +0200

    cart quantity

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -71,6 +71,11 @@ button.addEventListener('click', ()=>{
     else{
       cart.push({productId : productId,quantity:1});
     }
-    console.log(cart)
+    let cartQuantity=0;
+    cart.forEach((item)=>{
+      cartQuantity+=item.quantity;
+    });
+    document.querySelector(".js-cart-quantity").innerHTML=cartQuantity;
+  
 });
 });
