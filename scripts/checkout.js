78981386a3ed64a721e845092d3150c0bff7e829
zeyadbadawyy <zeyadbadawyy@gmail.com>
2026-03-06 15:15:40 +0200

    rendering

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -3,6 +3,7 @@ import{products} from '../data/products.js';
 import {formatCurrency} from './utils/money.js';
 import { deliveryOptions } from '../data/deliveryOptions.js';
 
+function renderOrderSummary(){
 let cartSummaryHTML='';
 cart.forEach((cartItem)=>{
   const productId=cartItem.productId;
@@ -21,16 +22,13 @@ if(productId === product.id)
     const deliveryDate=today.add(deliveryOption.deliveryDays,'days');
     const dateFormat=deliveryDate.format('dddd, MMMM D');
 
-
     cartSummaryHTML+=`<div class="cart-item-container js-cart-item-cotainer-${product.id}">
               <div class="delivery-date">
                 Delivery date: ${dateFormat}
               </div>
-
               <div class="cart-item-details-grid">
                 <img class="product-image"
                   src=${product.image}>
-
                 <div class="cart-item-details">
                   <div class="product-name">
                     ${product.name}
@@ -90,36 +88,9 @@ function deliveryOptionsHTML(product, cartItem){
   })
   return html;
 }
-/*
-                
-                  <div class="delivery-option">
-                    <input type="radio" checked
-                      class="delivery-option-input"
-                      name="delivery-option-${product.id}">
-                    <div>
-                      <div class="delivery-option-date">
-                        Tuesday, June 21
-                      </div>
-                      <div class="delivery-option-price">
-                        FREE Shipping
-                      </div>
-                    </div>
-                  </div>
-                  <div class="delivery-option">
-                    <input type="radio"
-                      class="delivery-option-input"
-                      name="delivery-option-${product.id}">
-                    <div>
-                      <div class="delivery-option-date">
-                        Wednesday, June 15
-                      </div>
-                      <div class="delivery-option-price">
-                        $4.99 - Shipping
-                      </div>
-                    </div>
-                  </div>*/ 
 
 document.querySelector('.js-order-summary').innerHTML=cartSummaryHTML;
+
 
 document.querySelectorAll(`.js-delete-link`).forEach((link)=>{
   link.addEventListener("click", ()=>{
@@ -133,5 +104,9 @@ document.querySelectorAll('.js-delivery-option').forEach((element)=>{
   element.addEventListener("click", ()=>{
     const{productId, deliveryOptionId}=element.dataset;
     updateDeliveryOption(productId,deliveryOptionId);
+    renderOrderSummary();
   })
 })
+}
+
+renderOrderSummary();
